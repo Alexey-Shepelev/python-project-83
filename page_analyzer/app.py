7efe83b29ad7d6b626efbed89af39b2227994654
Alexey-Shepelev <alexey.sailor@gmail.com>
2023-01-23 21:37:32 +0300

    Rev #4. Minor code refactoring.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -12,7 +12,7 @@ from psycopg2.extras import NamedTupleCursor
 from datetime import datetime
 from dotenv import load_dotenv
 import requests
-from .url import get_domain, is_valid
+from .url import get_domain, validate
 from .page import parse
 
 app = Flask(__name__)
@@ -49,10 +49,10 @@ def get_urls():
 @app.post('/urls')
 def add_url():
     raw_url = request.form.get('url')
-    errors = is_valid(raw_url)
-    if errors:
-        for error in errors:
-            flash(error, 'alert-danger')
+    alerts = validate(raw_url)
+    if alerts:
+        for alert in alerts:
+            flash(alert, 'alert-danger')
         return render_template(
             'index.html', url=raw_url), 422
     try:
--- page_analyzer/url.py
+++ page_analyzer/url.py
@@ -1,4 +1,4 @@
-from validators import url as validate
+from validators import url as valid
 from urllib.parse import urlparse
 
 
@@ -7,19 +7,19 @@ def get_domain(url):
     return f"{url.scheme}://{url.netloc}"
 
 
-def is_valid(raw_url):
+def validate(raw_url):
     """
     Check URL if it's correct (using validators.url),
     not empty and not more than 255 characters
     :param raw_url: URL
-    :return: List of errors
+    :return: List of alerts
     """
-    err_list = []
+    alerts = []
     url = get_domain(raw_url)
     if not raw_url:
-        err_list.append('URL обязателен')
-    if not validate(raw_url):
-        err_list.append('Некорректный URL')
+        alerts.append('URL обязателен')
+    if not valid(raw_url):
+        alerts.append('Некорректный URL')
     if len(url) > 255:
-        err_list.append('URL превышает 255 символов')
-    return err_list
+        alerts.append('URL превышает 255 символов')
+    return alerts
